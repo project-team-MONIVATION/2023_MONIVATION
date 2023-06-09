94b3cbdf78f3bf849a35518c894c88438ba802c6
cosch218 <cosch218@gmail.com>
2023-06-09 13:13:09 +0900

    0609 merge하기 위한 커밋

--- src/member_PCH/pages/Home.jsx
+++ src/member_PCH/pages/Home.jsx
@@ -1,29 +1,45 @@
 // 메인페이지 
 
 import React from 'react';
-import { Link } from 'react-router-dom';
-import IncomeModalComp from '../components/IncomeModalComp';
-import ExpenseModalComp from '../components/ExpenseModalComp';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { userLogout } from '../../member_PC_HS/slice/userSlice';
 
 export default function Home() {
+  const user = useSelector((state)=>state.user.user);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    console.log(sessionStorage.getItem('user'))
+    dispatch(userLogout());
+    navigate('/');
+  }
+
   return (
     <div>
       <h1>Home</h1>
       <div>
-        로그인 안 하면 보임
-        <br />
-        <Link to='/account/login'>로그인</Link>
-        <Link to='/account/create'>회원가입</Link>
-      </div>
-      <br />
-      <div>
-        로그인 하면 보임
-        <br />
-        <Link to='/calendar'>가계부</Link>
-        <Link to='/challenge'>챌린지</Link>
-        <Link to='/asset'>자산관리</Link>
-        <Link to='/mypage'>마이페이지</Link>
-        <Link to='/'>로그아웃</Link>
+        {
+          user === null
+          ? (
+            <div>
+              <Link to='/account/login'>로그인</Link>
+              <Link to='/account/create'>회원가입</Link>
+            </div>
+          )
+          : (
+            
+            <div>
+            <Link to='/calendar'>가계부</Link>
+            <Link to='/challenge'>챌린지</Link>
+            <Link to='/asset'>자산관리</Link>
+            <Link to='/mypage'>마이페이지</Link>
+            <button onClick={ onLogout }>로그아웃</button>
+          </div>
+          )
+        }
       </div>
     </div>
   )
