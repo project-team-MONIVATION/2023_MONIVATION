3b6a30dae67ed85806dee8caf4794b459d5e4438
chhann <wjdcks2252@naver.com>
2023-06-12 19:47:16 +0900

    2023-06-12 각 컴포넌트 연결완료

--- src/member_JSH/components/TotalStatComp.jsx
+++ src/member_JSH/components/TotalStatComp.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux';
+import { useNavigate  } from 'react-router-dom';
+import SavingList from '../../member_LJC/pages/SavingList';
+
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc, query, where, Timestamp, } from 'firebase/firestore';
+import {db} from '../../database/firebase'
+import Modal from 'react-modal';
+
 
 export default function TotalStatComp() {
   // 일반 수입 리덕스
@@ -10,6 +17,75 @@ export default function TotalStatComp() {
   const exlist = useSelector((state)=>(state.ex));
   // 저금 리덕스ㅇddd
   const savelist = useSelector((state)=>(state.save));
+
+
+
+  
+  // 저금 리스트 모달창
+  const navigate = useNavigate();
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const [activeModal, setActiveModal] = useState(null);
+    const openModal = (modalId) => {
+      setActiveModal(modalId);
+    };
+
+
+  // 총 저금액 불러오기
+  const [totalamount, setTotalamount] = useState([]);
+
+  const user = useSelector((state) => state.user.user);
+
+  const [savingList , setSavingList] = useState([]);
+
+  useEffect(() => {
+      getSavingData();
+  }, [user]);
+
+
+  // 데이터 불러오기
+  // 저금액 총액 구하기
+  const getSavingData = async () => {
+      try {
+          const fmCollectionRef = collection(db, "money_saving");
+          const fmQuery = query(fmCollectionRef, where('user', '==', user.uid));
+          const fmQuerySnapshot = await getDocs(fmQuery);
+
+          if (fmQuerySnapshot.empty) {
+              navigate('/account/login');
+          } else {
+              let dataArray = [];
+              fmQuerySnapshot.forEach((doc) => {
+                  dataArray.push({
+                  ...doc.data(),
+                  id: doc.id,
+              });
+              });
+              let total = 0;
+
+              for (let i = 0; i < dataArray.length; i++) {
+                let money = dataArray[i].amount;
+                const number = parseInt(money.replace(/,/g, ''), 10);
+                total += number;
+                
+              }
+              const totalprice = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+              setSavingList(totalprice);
+          }
+      } catch (error) {
+          console.log("실패했습니다", error);
+      }
+  };
+
+  
+
+
+  
+
+
+
+
   return (
     <div style={{height : "538px", 
     display : 'inline-block', 
@@ -43,12 +119,30 @@ export default function TotalStatComp() {
             <tr>
               <td>- 0,000,000원</td>
             </tr>
-            <tr>
-              <td>현재 총 저금액</td>
-            </tr>
-            <tr>
-              <td>0,000,000원</td>
-            </tr>
+
+            {/* 총 저금액 시작*/}
+              <div
+                onClick={()=>{
+                  setModalIsOpen(true);
+                  openModal(3);
+                }}
+              >
+                <tr>
+                  <td>현재 총 저금액</td>
+                </tr>
+                <tr>
+                  <td>-{savingList}원</td>
+                </tr>
+              </div>
+              {activeModal === 3 && (
+                <Modal isOpen={modalIsOpen}>
+                  <div>
+                  <button onClick={()=>setModalIsOpen(false)}>x</button>
+                  <SavingList setModalIsOpen={setModalIsOpen}/>
+                  </div>
+                </Modal>
+              )}
+          {/* 총 저금액 끝 */}
             <tr>
               <td>현재 총 자산</td>
             </tr>
