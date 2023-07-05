c4adc72e4d7308d0409218c4eef6bc11a1078c3f
cosch218 <cosch218@gmail.com>
2023-07-05 16:50:06 +0900

    0705 디테일 수정

--- src/member_PC/pages/styledComponent/AssetField.jsx
+++ src/member_PC/pages/styledComponent/AssetField.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const FieldDiv = styled.div`
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
 `
 
